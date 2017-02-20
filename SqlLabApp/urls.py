9e5407389145a179dec642d3bc6c9aa8c21e78dd
Daryl Tan <daryltanzw@gmail.com>
2017-02-20 17:23:07 +0800

    Database setup

--- SqlLabApp/urls.py
+++ SqlLabApp/urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 
 import views
 
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
