4f180801b88e96acde48341115a9945c126ed50b
jun159 <baojun159@gmail.com>
2017-03-20 00:12:25 +0800

    Able to retrieve classid and pass to create test form

--- SqlLabApp/views/instructortest.py
+++ SqlLabApp/views/instructortest.py
@@ -1,8 +1,21 @@
-from django.views.generic import DetailView
+from django.views.generic import FormView
+from SqlLabApp.forms.instructortest import InstructorTestForm
 
-from SqlLabApp.models import Class
+from SqlLabApp.models import Class, TestForClass
 
-
-class InstructorTestFormView(DetailView):
+class InstructorTestFormView(FormView):
     model = Class
+    form_class = InstructorTestForm
     template_name = 'SqlLabApp/instructortest.html'
+    success_url = '/'
+
+    def get(self, request, *args, **kwargs):
+        create_module_form = InstructorTestForm
+        test_list = TestForClass.objects.filter(classid_id=self.kwargs['pk'])
+
+        return self.render_to_response(
+            self.get_context_data(
+                test_list=test_list,
+                create_module_form=create_module_form,
+            )
+        )
