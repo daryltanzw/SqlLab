260c07157940c48e9758b45ac03f98c1cb4b60d3
jun159 <baojun159@gmail.com>
2017-03-21 22:54:34 +0800

    Removed redundant file

--- SqlLabApp/forms/createtest.py
+++ SqlLabApp/forms/createtest.py
@@ -1,17 +1,29 @@
+import datetime
+
 from django import forms
 
 
 class CreateTestForm(forms.Form):
-    test_name = forms.CharField(widget=forms.widgets.TextInput(attrs={'placeholder': 'Test Name'}), label='')
-    start_time = forms.DateField(widget=forms.widgets.DateInput(attrs={'class': 'datepicker', 'placeholder': 'Start Date'}), label='')
-    end_time = forms.DateField(widget=forms.widgets.DateInput(attrs={'class': 'datepicker', 'placeholder': 'End Date'}), label='')
+    test_name = forms.CharField(widget=forms.widgets.TextInput(attrs={}), label='Test Name')
+    start_time = forms.DateField(widget=forms.widgets.DateInput(attrs={'class': 'datepicker'}), label='Start Date')
+    end_time = forms.DateField(widget=forms.widgets.DateInput(attrs={'class': 'datepicker'}), label='End Date')
     max_attempt = forms.IntegerField(widget=forms.widgets.NumberInput(
-                    attrs={'placeholder': 'Number of Attempts Allowed (Leave Blank for Unlimited)'}),
-                    label='', initial=None)
+                    attrs={}),
+                    label='Number of Attempts (Leave Blank for Unlimited)', initial=None, required=False)
     q_a_file_upload = forms.FileField(label="Upload Tab Separated File ( Question <tab> Answer )")
     data_file_upload = forms.FileField(label="Upload .sql dumps")
 
     class Meta:
         fields = ['test_name', 'max_attempt', 'start_time', 'end_time', 'q_a_file_upload', 'data_file_upload']
 
-
+    # def clean(self):
+    #     start_time = self.data.get('start_time')
+    #     end_time = self.data.get('end_time')
+    #
+    #     if start_time < datetime.date.today() | end_time < datetime.date.today():
+    #         raise forms.ValidationError("The date cannot be in the past!")
+    #
+    #     if start_time >= end_time :
+    #         raise forms.ValidationError("Start Date should be before End Date!")
+    #
+    #     return start_time
