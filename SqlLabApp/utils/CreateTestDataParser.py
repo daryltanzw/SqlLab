d26b0f394fa3eebd48eb002ee213e240936bf61a
Daryl Tan <daryltanzw@gmail.com>
2017-03-22 14:27:58 +0800

    refractor and abstraction so as to improve reusability

--- SqlLabApp/utils/CreateTestDataParser.py
+++ SqlLabApp/utils/CreateTestDataParser.py
@@ -5,14 +5,8 @@ from TestNameTableFormatter import test_name_table_format
 
 def append_to_relations(tid, query_line_list):
     ret = []
-    query = '\n'.join(query_line_list)
-    result_list = sqlparse.parse(sqlparse.format(query, reindent=True, keyword_case='upper'))
-    table_dict = {}
-
-    for result in result_list:
-        if result.get_type() == 'CREATE':
-            tbl_name = result.get_name()
-            table_dict[tbl_name] = str(test_name_table_format(tid, tbl_name))
+    table_name_list = get_tbl_names(query_line_list)
+    table_dict = table_name_to_formatted(tid, table_name_list)
 
     for line in query_line_list:
         for key in table_dict.keys():
@@ -22,3 +16,25 @@ def append_to_relations(tid, query_line_list):
         ret.append(str(line))
 
     return ret
+
+
+def get_tbl_names(query_line_list):
+    query = '\n'.join(query_line_list)
+    result_list = sqlparse.parse(sqlparse.format(query, reindent=True, keyword_case='upper'))
+
+    table_name_list = []
+
+    for result in result_list:
+        if result.get_type() == 'CREATE':
+            tbl_name = result.get_name()
+            table_name_list.append(tbl_name)
+
+    return table_name_list
+
+
+def table_name_to_formatted(tid, table_name_list):
+    table_dict = {}
+    for name in table_name_list:
+        table_dict[name] = str(test_name_table_format(tid, name))
+
+    return table_dict
