26c582c009100487e9a6c8338dc9e524b1f7c248
Daryl Tan <daryltanzw@gmail.com>
2017-03-22 17:56:52 +0800

    change from '.' to '_' as tid1.tblName is recognized as schema and throws error

--- SqlLabApp/test/file/ExpectedTestData1.sql
+++ SqlLabApp/test/file/ExpectedTestData1.sql
@@ -1,4 +1,4 @@
-CREATE TABLE tid1.warehouse(
+CREATE TABLE tid1_warehouse(
 	w_id INTEGER PRIMARY KEY,
 	w_name VARCHAR(50),
 	w_street VARCHAR(50),
@@ -7,13 +7,13 @@ CREATE TABLE tid1.warehouse(
 );
 
 
-INSERT INTO tid1.warehouse (w_id, w_name, w_street, w_city, w_country) VALUES (301, 'Schmedeman', 'Sunbrook', 'Singapore', 'Singapore');
-INSERT INTO tid1.warehouse (w_id, w_name, w_street, w_city, w_country) VALUES (281, 'Crescent Oaks', 'Loeprich', 'Singapore', 'Singapore');
-INSERT INTO tid1.warehouse (w_id, w_name, w_street, w_city, w_country) VALUES (22, 'Namekagon', 'Anniversary', 'Singapore', 'Singapore');
-INSERT INTO tid1.warehouse (w_id, w_name, w_street, w_city, w_country) VALUES (1004, 'Fairfield', 'Sachs', 'Singapore', 'Singapore');
-INSERT INTO tid1.warehouse (w_id, w_name, w_street, w_city, w_country) VALUES (3, 'Briar Crest', 'Kensington', 'Singapore', 'Singapore');
+INSERT INTO tid1_warehouse (w_id, w_name, w_street, w_city, w_country) VALUES (301, 'Schmedeman', 'Sunbrook', 'Singapore', 'Singapore');
+INSERT INTO tid1_warehouse (w_id, w_name, w_street, w_city, w_country) VALUES (281, 'Crescent Oaks', 'Loeprich', 'Singapore', 'Singapore');
+INSERT INTO tid1_warehouse (w_id, w_name, w_street, w_city, w_country) VALUES (22, 'Namekagon', 'Anniversary', 'Singapore', 'Singapore');
+INSERT INTO tid1_warehouse (w_id, w_name, w_street, w_city, w_country) VALUES (1004, 'Fairfield', 'Sachs', 'Singapore', 'Singapore');
+INSERT INTO tid1_warehouse (w_id, w_name, w_street, w_city, w_country) VALUES (3, 'Briar Crest', 'Kensington', 'Singapore', 'Singapore');
 
-CREATE TABLE tid1.warehouse1 (
+CREATE TABLE tid1_warehouse1 (
 	w_id INTEGER PRIMARY KEY,
 	w_name VARCHAR(50),
 	w_street VARCHAR(50),
@@ -21,16 +21,16 @@ CREATE TABLE tid1.warehouse1 (
 	w_country VARCHAR(50),
 );
 
-INSERT INTO tid1.warehouse1 (w_id, w_name, w_street, w_city, w_country) VALUES (1, 'DabZ', 'Green', 'Patemon', 'Indonesia');
-INSERT INTO tid1.warehouse1 (w_id, w_name, w_street, w_city, w_country) VALUES (2, 'Skippad', 'Harper', 'Makale', 'Indonesia');
-INSERT INTO tid1.warehouse1 (w_id, w_name, w_street, w_city, w_country) VALUES (1005, 'Eare', 'Dahle', 'Padangbatung', 'Indonesia');
-INSERT INTO tid1.warehouse1 (w_id, w_name, w_street, w_city, w_country) VALUES (4, 'Topiczoom', 'John Wall', 'Pagaden', 'Indonesia');
-INSERT INTO tid1.warehouse1 (w_id, w_name, w_street, w_city, w_country) VALUES (5, 'Wordpedia', 'Ridgeview', 'Sumberjati', 'Indonesia');
-INSERT INTO tid1.warehouse1 (w_id, w_name, w_street, w_city, w_country) VALUES (6, 'Wordify', 'Toban', 'Banjar Serangan', 'Indonesia');
-INSERT INTO tid1.warehouse1 (w_id, w_name, w_street, w_city, w_country) VALUES (7, 'Blogpad', 'Monica', 'Nusajaya', 'Malaysia');
-INSERT INTO tid1.warehouse1 (w_id, w_name, w_street, w_city, w_country) VALUES (8, 'Bluezoom', 'Atwood', 'Lunyuk Ode', 'Indonesia');
+INSERT INTO tid1_warehouse1 (w_id, w_name, w_street, w_city, w_country) VALUES (1, 'DabZ', 'Green', 'Patemon', 'Indonesia');
+INSERT INTO tid1_warehouse1 (w_id, w_name, w_street, w_city, w_country) VALUES (2, 'Skippad', 'Harper', 'Makale', 'Indonesia');
+INSERT INTO tid1_warehouse1 (w_id, w_name, w_street, w_city, w_country) VALUES (1005, 'Eare', 'Dahle', 'Padangbatung', 'Indonesia');
+INSERT INTO tid1_warehouse1 (w_id, w_name, w_street, w_city, w_country) VALUES (4, 'Topiczoom', 'John Wall', 'Pagaden', 'Indonesia');
+INSERT INTO tid1_warehouse1 (w_id, w_name, w_street, w_city, w_country) VALUES (5, 'Wordpedia', 'Ridgeview', 'Sumberjati', 'Indonesia');
+INSERT INTO tid1_warehouse1 (w_id, w_name, w_street, w_city, w_country) VALUES (6, 'Wordify', 'Toban', 'Banjar Serangan', 'Indonesia');
+INSERT INTO tid1_warehouse1 (w_id, w_name, w_street, w_city, w_country) VALUES (7, 'Blogpad', 'Monica', 'Nusajaya', 'Malaysia');
+INSERT INTO tid1_warehouse1 (w_id, w_name, w_street, w_city, w_country) VALUES (8, 'Bluezoom', 'Atwood', 'Lunyuk Ode', 'Indonesia');
 
-CREATE TABLE tid1.warehouse2 (
+CREATE TABLE tid1_warehouse2 (
 	w_id INTEGER PRIMARY KEY,
 	w_name VARCHAR(50),
 	w_street VARCHAR(50),
@@ -38,15 +38,15 @@ CREATE TABLE tid1.warehouse2 (
 	w_country VARCHAR(50),
 );
 
-INSERT INTO tid1.warehouse2 (w_id, w_name, w_street, w_city, w_country) VALUES (9, 'Latz', 'Independence', 'Sadang Kulon', 'Indonesia');
-INSERT INTO tid1.warehouse2 (w_id, w_name, w_street, w_city, w_country) VALUES (10, 'Cogibox', 'Declaration', 'Sambong', 'Indonesia');
-INSERT INTO tid1.warehouse2 (w_id, w_name, w_street, w_city, w_country) VALUES (11, 'Skyble', 'Utah', 'Andekantor', 'Indonesia');
-INSERT INTO tid1.warehouse2 (w_id, w_name, w_street, w_city, w_country) VALUES (12, 'Kanoodle', 'Park Meadow', 'Pembuanghulu', 'Indonesia');
-INSERT INTO tid1.warehouse2 (w_id, w_name, w_street, w_city, w_country) VALUES (13, 'Voomm', 'Luster', 'Ayamaru', 'Indonesia');
-INSERT INTO tid1.warehouse2 (w_id, w_name, w_street, w_city, w_country) VALUES (14, 'Mydo', 'Maywood', 'Andongrejo', 'Indonesia');
-INSERT INTO tid1.warehouse2 (w_id, w_name, w_street, w_city, w_country) VALUES (15, 'Wordify', 'Anniversary', 'Gununganyartambak Tengah', 'Indonesia');
+INSERT INTO tid1_warehouse2 (w_id, w_name, w_street, w_city, w_country) VALUES (9, 'Latz', 'Independence', 'Sadang Kulon', 'Indonesia');
+INSERT INTO tid1_warehouse2 (w_id, w_name, w_street, w_city, w_country) VALUES (10, 'Cogibox', 'Declaration', 'Sambong', 'Indonesia');
+INSERT INTO tid1_warehouse2 (w_id, w_name, w_street, w_city, w_country) VALUES (11, 'Skyble', 'Utah', 'Andekantor', 'Indonesia');
+INSERT INTO tid1_warehouse2 (w_id, w_name, w_street, w_city, w_country) VALUES (12, 'Kanoodle', 'Park Meadow', 'Pembuanghulu', 'Indonesia');
+INSERT INTO tid1_warehouse2 (w_id, w_name, w_street, w_city, w_country) VALUES (13, 'Voomm', 'Luster', 'Ayamaru', 'Indonesia');
+INSERT INTO tid1_warehouse2 (w_id, w_name, w_street, w_city, w_country) VALUES (14, 'Mydo', 'Maywood', 'Andongrejo', 'Indonesia');
+INSERT INTO tid1_warehouse2 (w_id, w_name, w_street, w_city, w_country) VALUES (15, 'Wordify', 'Anniversary', 'Gununganyartambak Tengah', 'Indonesia');
 
-CREATE TABLE tid1.warehouse3 (
+CREATE TABLE tid1_warehouse3 (
 	w_id INTEGER PRIMARY KEY,
 	w_name VARCHAR(50),
 	w_street VARCHAR(50),
@@ -54,12 +54,12 @@ CREATE TABLE tid1.warehouse3 (
 	w_country VARCHAR(50),
 );
 
-INSERT INTO tid1.warehouse3 (w_id, w_name, w_street, w_city, w_country) VALUES (16, 'Kimia', 'Tennessee', 'Cikiruh Wetan', 'Indonesia');
-INSERT INTO tid1.warehouse3 (w_id, w_name, w_street, w_city, w_country) VALUES (17, 'Yakitri', 'Annamark', 'Barengkok', 'Indonesia');
-INSERT INTO tid1.warehouse3(w_id, w_name, w_street, w_city, w_country) VALUES (18, 'Dazzlesphere', 'Village', 'Rukem', 'Indonesia');
-INSERT INTO tid1.warehouse3 (w_id, w_name, w_street, w_city, w_country) VALUES (19, 'Jetpulse', 'Monterey', 'Woja', 'Indonesia');
-INSERT INTO tid1.warehouse3 (w_id, w_name, w_street, w_city, w_country) VALUES (20, 'Meetz', 'Moland', 'Gareba', 'Indonesia');
-INSERT INTO tid1.warehouse3 (w_id, w_name, w_street, w_city, w_country) VALUES (21, 'Brainbox', 'Bowman', 'Winong', 'Indonesia');
-INSERT INTO tid1.warehouse3 (w_id, w_name, w_street, w_city, w_country) VALUES (1003, 'Oba', 'Washington', 'Kulonkali', 'Indonesia');
-INSERT INTO tid1.warehouse3 (w_id, w_name, w_street, w_city, w_country) VALUES (23, 'Avamm', 'Lakewood', 'Imaan', 'Indonesia');
-INSERT INTO tid1.warehouse3 (w_id, w_name, w_street, w_city, w_country) VALUES (24, 'Kamba', 'Hanson', 'Panjerrejo', 'Indonesia');
+INSERT INTO tid1_warehouse3 (w_id, w_name, w_street, w_city, w_country) VALUES (16, 'Kimia', 'Tennessee', 'Cikiruh Wetan', 'Indonesia');
+INSERT INTO tid1_warehouse3 (w_id, w_name, w_street, w_city, w_country) VALUES (17, 'Yakitri', 'Annamark', 'Barengkok', 'Indonesia');
+INSERT INTO tid1_warehouse3(w_id, w_name, w_street, w_city, w_country) VALUES (18, 'Dazzlesphere', 'Village', 'Rukem', 'Indonesia');
+INSERT INTO tid1_warehouse3 (w_id, w_name, w_street, w_city, w_country) VALUES (19, 'Jetpulse', 'Monterey', 'Woja', 'Indonesia');
+INSERT INTO tid1_warehouse3 (w_id, w_name, w_street, w_city, w_country) VALUES (20, 'Meetz', 'Moland', 'Gareba', 'Indonesia');
+INSERT INTO tid1_warehouse3 (w_id, w_name, w_street, w_city, w_country) VALUES (21, 'Brainbox', 'Bowman', 'Winong', 'Indonesia');
+INSERT INTO tid1_warehouse3 (w_id, w_name, w_street, w_city, w_country) VALUES (1003, 'Oba', 'Washington', 'Kulonkali', 'Indonesia');
+INSERT INTO tid1_warehouse3 (w_id, w_name, w_street, w_city, w_country) VALUES (23, 'Avamm', 'Lakewood', 'Imaan', 'Indonesia');
+INSERT INTO tid1_warehouse3 (w_id, w_name, w_street, w_city, w_country) VALUES (24, 'Kamba', 'Hanson', 'Panjerrejo', 'Indonesia');
--- SqlLabApp/utils/TestNameTableFormatter.py
+++ SqlLabApp/utils/TestNameTableFormatter.py
@@ -1,3 +1,3 @@
 
 def test_name_table_format(tid, tbl_name):
-    return "tid" + str(tid) + "." + tbl_name
+    return str("tid" + str(tid) + "_" + tbl_name)
