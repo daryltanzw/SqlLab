ba41d31f9b85d7fdc62ebe40bcd2e9677f0fbc88
GVishnuP <vishki.priya@gmail.com>
2017-03-28 15:08:16 +0800

    Add tests for correctness checker

--- SqlLabApp/test/test_CorrectnessChecker.py
+++ SqlLabApp/test/test_CorrectnessChecker.py
@@ -12,7 +12,6 @@ class CorrectnessCheckerTest(TestCase):
     @classmethod
     def setUpClass(cls):
         connection = get_db_connection()
-        statements = []
         with open('SqlLabApp/test/file/CorrectnessCheckerData.sql', 'r') as script:
             for statement in script:
                 connection.cursor().execute(statement)
@@ -35,6 +34,20 @@ class CorrectnessCheckerTest(TestCase):
     def testSelectEqualsJoinThreeTables(self):
         query1 = "SELECT i_name, s_qty from item i, stock s, warehouse w where i.i_id = s.i_id and w.w_id = s.w_id and w.w_id = 1"
         query2 = "SELECT i_name, s_qty from item i natural join stock s natural join warehouse w where w.w_id = 1"
+        correctnessChecker = CorrectnessChecker()
+        result = correctnessChecker.compare(query1, query2)
+        self.assertEqual(result, 0)
+
+    def testNestedInSelectEquals(self):
+        query1 = "Select i.i_id, i.i_name, i.i_price from item i where i.i_id IN (Select s.i_id from stock s where s.w_id = 1) and i.i_id not in (Select s1.i_id from stock s1 where s1.w_id = 3) "
+        query2 = "Select i.i_id, i.i_name, i.i_price from item i, stock s where i.i_id = s.i_id and s.w_id = 1 except Select i.i_id, i.i_name, i.i_price from item i, stock s where i.i_id = s.i_id and s.w_id = 3"
+        correctnessChecker = CorrectnessChecker()
+        result = correctnessChecker.compare(query1, query2)
+        self.assertEqual(result, 0)
+
+    def testSelectGroupBysEquals(self):
+        query1 = "Select count(s.i_id), s.w_id from stock s group by s.w_id"
+        query2 = "Select count(s.i_id), s.w_id from stock s group by s.w_id"
         correctnessChecker = CorrectnessChecker()
         result = correctnessChecker.compare(query1, query2)
         self.assertEqual(result, 0)
