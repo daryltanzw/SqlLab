e6f81c1e112ff590864bc3c2262d3c08027f7657
jun159 <baojun159@gmail.com>
2017-03-29 00:57:18 +0800

    Create student attempt table

---
+++ SqlLabApp/utils/CreateStudentAttemptTable.py
@@ -0,0 +1,38 @@
+import itertools as it
+
+from SqlLabApp.models import TestForClass
+from SqlLabApp.utils.CorrectnessChecker import compare
+from SqlLabApp.utils.TestNameTableFormatter import test_name_table_format
+
+def create_student_attempt_table(cursor, tbl_name, tid, student_answer_list):
+    try:
+        cursor.execute("drop table if exists {0};".format(tbl_name))
+        cursor.execute(
+            "create table {0} (qid INTEGER primary key, question text, student_answer text, marks INTEGER);".format(tbl_name))
+
+        q_no = 0
+        for student_answer in student_answer_list:
+            q_no += 1
+            test_name = TestForClass.objects.get(tid=tid).test_name
+            test_table = test_name_table_format(tid, test_name).lower()
+
+            # Retrieve question from test table
+            cursor.execute('select question, teacher_answer, marks from ' + test_table + ' where qid = ' + str(q_no))
+            tuple_list = cursor.fetchall()
+            question = [i[0] for i in it.chain(tuple_list)][0]
+            instructor_answer = [i[1] for i in it.chain(tuple_list)][0]
+            total_marks = [i[2] for i in it.chain(tuple_list)][0]
+
+            # Mark student's query against instructor's query
+            # TODO: Replace query's table names with actual table names stored
+            correctness = compare(instructor_answer.replace(";", ""), student_answer.replace(";", ""))
+            student_marks = 0
+
+            # Correct = full marks, wrong = 0
+            if correctness == 0:
+                student_marks = total_marks
+
+            cursor.execute("Insert into {0} values(%s, %s, %s, %s)".format(tbl_name), (q_no, question, student_answer, student_marks))
+
+    except Exception as err:
+        raise ValueError(err)
--- SqlLabApp/utils/TestNameTableFormatter.py
+++ SqlLabApp/utils/TestNameTableFormatter.py
@@ -1,3 +1,6 @@
 
 def test_name_table_format(tid, tbl_name):
     return str("tid" + str(tid) + "_" + tbl_name.replace("-", "").replace(" ", ""))
+
+def student_attempt_table_format(tid, student_email, attempt_no):
+    return str("tid" + str(tid) + "_" + str(student_email).replace("@", "").replace(".", "") + "_" + str(attempt_no))
