aeedace47a19a8e3531d07ab5da0b745d4b3b754
Daryl Tan <daryltanzw@gmail.com>
2017-04-03 01:14:17 +0800

    tidy up format of files

--- SqlLabApp/views.py
+++
@@ -1,7 +0,0 @@
-# Boolean CreateTest(files*):
-#     # create Test table entry
-#     # Run sql Script - make sure the data tables are created
-#     # Daryl - CreateQuestionTable
-#
-# Daryl- Boolean CreateQuestionTable(tblName, csvFile):
-#     # i would have created the dynamic question database with Question and Answer
