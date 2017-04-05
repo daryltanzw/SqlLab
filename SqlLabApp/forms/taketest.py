57560c1fb176b7fb76d2c132b5f6c5795bad8035
jun159 <baojun159@gmail.com>
2017-04-05 22:05:39 +0800

    Fixed submit button in take test

--- SqlLabApp/forms/taketest.py
+++ SqlLabApp/forms/taketest.py
@@ -3,7 +3,7 @@ from django import forms
 
 class TakeTestForm(forms.Form):
     
-    student_answer = forms.CharField(widget=forms.Textarea, label='')
+    student_answer = forms.CharField(widget=forms.Textarea, label='', required=True)
 
 
 
